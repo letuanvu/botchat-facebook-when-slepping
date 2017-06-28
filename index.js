55d7676496a9ebd770906f899af238c2ccaf4d12
ltvu59 <ltvu.59@gmail.com>
2017-06-28 18:14:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const login = require("facebook-chat-api");
+const $ = require("jquery");
+
+var answeredThreads = [];
+
+login({email: "happysmile1059@gmail.com", password: "letuanvu95"}, function callback (err, api) {
+    if(err) return console.error(err);
+
+
+    api.listen(function callback(err, message) {
+        var d = new Date();
+        var h = d.getHours();
+
+        if((h >= 21 || h <= 6)){
+            api.getUserInfo(message.senderID, function(err, ret) {
+                if(err) return console.error(err);
+
+                for(var prop in ret) {
+                    if(ret.hasOwnProperty(prop) && ret[prop].name) {
+                        switch (answeredThreads[message.threadID]){
+                            case undefined:
+                                api.sendMessage( "BOT: Xin chào " + ret[prop].name + " cũng muộn rồi, có gì để trời sáng đã nhé!", prop, function(){
+                                    if(!!answeredThreads[message.threadID]){
+                                        answeredThreads[message.threadID]++;
+                                    }else{
+                                        answeredThreads[message.threadID] = 2;
+                                    }
+                                });
+                                break;
+
+                            case 1:
+                                api.sendMessage( "BOT: Xin chào " + ret[prop].name + " cũng muộn rồi, có gì để trời sáng đã nhé!", prop, function(){
+                                    answeredThreads[message.threadID]++;
+                                });
+                                break;
+                            case 2:
+                                api.sendMessage( "BOT: Chúc " + ret[prop].name + " ngủ ngon nhé!", prop, function(){
+                                    answeredThreads[message.threadID]++;
+                                });
+                                break;
+                            case 3:
+                                api.sendMessage( "BOT: zZ!", prop, function(){
+                                    answeredThreads[message.threadID]++;
+                                });
+                                break;
+                            default: break;
+                        }
+                    }
+                }
+
+            });
+        }else{
+            answeredThreads = [];
+        }
+    });
+});
